673c6e1d9e25ac0b7aa24bc1dbc616f87b19937c
nwelter1 <nwelter1@gmail.com>
2020-11-04 10:36:57 -0600

    flask api - chicodes

--- env_test.py
+++ env_test.py
@@ -6,7 +6,7 @@ import random
 
 def add_to_env(length):
     with open('.env','w+') as file:
-        test = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user='postgres',pw='LittleZoeNugget3689',url='127.0.0.1:5433',db='api_test_db')
+        test = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user='postgres',pw='PasswordHere',url='127.0.0.1:5433',db='api_test_db')
         data = file.read()
         file.seek(0)
         all_letters = string.ascii_lowercase
